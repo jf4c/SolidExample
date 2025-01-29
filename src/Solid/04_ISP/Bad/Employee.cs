061cd74a66bb057c48b7894d340b2cb7d4a3a1bd
Julio Felipe <julio.f4costa@hotmail.com>
2025-01-29 09:18:23 -0300

    Create example ISP

---
+++ src/Solid/04_ISP/Bad/Employee.cs
@@ -0,0 +1,36 @@
+﻿namespace Solid._04_ISP.Bad;
+
+public interface IEmployee
+{
+    string Name { get; set; }
+    void CalculateSalary();
+    void CalculeteBenefits();
+}
+
+public class FullTimeEmployee : IEmployee
+{
+    public string Name { get; set; } = string.Empty;
+    public void CalculateSalary()
+    {
+        Console.WriteLine("contract salary");
+    }
+
+    public void CalculeteBenefits()
+    {
+        Console.WriteLine("contract benefits");
+    }
+}
+
+public class ContractEmployee : IEmployee
+{
+    public string Name { get; set; } = string.Empty;
+    public void CalculateSalary()
+    {
+        Console.WriteLine("contract salary");
+    }
+
+    public void CalculeteBenefits()
+    {
+        Console.WriteLine("contract benefits");
+    }
+}
---
+++ src/Solid/04_ISP/Good/Employee.cs
@@ -0,0 +1,32 @@
+﻿namespace Solid._04_ISP.Good;
+
+public interface ISalaryEmployee
+{
+    void CalculateSalary();
+}
+    
+public interface IBenefitsEmployee
+{
+    void CalculeteBenefits();
+}
+
+public class FullTimeEmployee : ISalaryEmployee, IBenefitsEmployee
+{
+    public void CalculateSalary()
+    {
+        Console.WriteLine("contract salary");
+    }
+
+    public void CalculeteBenefits()
+    {
+        Console.WriteLine("contract benefits");
+    }
+}
+
+public class ContractEmployee : IBenefitsEmployee
+{
+    public void CalculeteBenefits()
+    {
+        Console.WriteLine("contract benefits");
+    }
+}
