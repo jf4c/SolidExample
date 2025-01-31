6233f33c92e196e4a485c08e2e00a0fdacc24d27
Julio Felipe <julio.f4costa@hotmail.com>
2025-01-30 21:43:46 -0300

    Create example DIP

---
+++ src/Solid/05_DIP/Bad/EmailService.cs
@@ -0,0 +1,12 @@
+﻿namespace Solid._05_DIP.Bad;
+
+public class EmailService
+{
+    public void Send()
+    {
+    } 
+}
+
+public class UserService(EmailService service)
+{
+}
---
+++ src/Solid/05_DIP/Good/EmailService.cs
@@ -0,0 +1,22 @@
+﻿namespace Solid._05_DIP.Good;
+
+interface IEmailService
+{
+    void Send();
+}
+
+public class EmailService : IEmailService
+{
+    public void Send()
+    {
+        Console.WriteLine("Sending email");
+    }
+}
+
+public class FakeEmailService : IEmailService
+{
+    public void Send()
+    {
+        Console.WriteLine("Sending FAKE email");
+    }
+}
